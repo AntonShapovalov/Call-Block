f33778067d9b3d2b661f2f74a5e1605b9836f761
AntonShapovalov <anton.shapovalov.5@gmail.com>
2015-05-24 23:08:17 +0300

    initial version
    Add initial code for Call Block application

---
+++ src/ru/org/adons/cblock/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.org.adons.cblock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getSupportActionBar().setDisplayShowHomeEnabled(true);
+		getSupportActionBar().setIcon(R.drawable.ic_launcher);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.activity_main_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+}
