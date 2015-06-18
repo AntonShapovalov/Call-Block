2a121b3600b996e2f4bff47ae0aa8e62f59d0d74
AntonShapovalov <anton.shapovalov.5@gmail.com>
2015-06-18 22:58:31 +0300

    customize action bar

--- app/src/main/java/ru/org/adons/cblock/MainActivity.java
+++ app/src/main/java/ru/org/adons/cblock/MainActivity.java
@@ -1,26 +1,41 @@
 package ru.org.adons.cblock;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatListActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		getSupportActionBar().setDisplayShowHomeEnabled(true);
-		getSupportActionBar().setIcon(R.drawable.ic_launcher);
-		setContentView(R.layout.activity_main);
-		
-		
-	}
+    private static final String LOG_TAG = "CBLOCK";
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu items for use in the action bar
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.activity_main_actions, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void startService(MenuItem item) {
+        int color;
+        Random r = new Random();
+        if (r.nextInt(10) % 2 == 0) {
+            color = R.color.green01;
+        } else {
+            color = R.color.orange01;
+        }
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(color));
+        Log.d(LOG_TAG, "startService");
+    }
 }
