05a06ea97702bd5f28743f5bf6deb867614456fb
AntonShapovalov <AntonShapovalov@users.noreply.github.com>
2015-07-06 18:43:35 +0300

    start/stop service handle

--- app/src/main/java/ru/org/adons/cblock/MainActivity.java
+++ app/src/main/java/ru/org/adons/cblock/MainActivity.java
@@ -3,6 +3,7 @@ package ru.org.adons.cblock;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -17,6 +18,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import java.util.Date;
 
@@ -28,8 +30,12 @@ import ru.org.adons.cblock.db.PhonesTable;
 public class MainActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = "CBLOCK";
+    // Action Bar and Action Button 'Start/Stop Service'
     private ActionBar actionBar;
-    private boolean swState;
+    private SharedPreferences pref;
+    private boolean isServiceEnabled;
+    private Intent serviceIntent;
+    // Auto Text View for Incoming call
     private AutoCompleteTextView incomingPhoneView;
 
     @Override
@@ -41,7 +47,11 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_main);
 
-        // set Start/Stop Service button
+        // set background for Action Bar and Action Button 'Start/Stop Service'
+        pref = getSharedPreferences(getString(R.string.app_preferences_file), MODE_PRIVATE);
+        isServiceEnabled = pref.getBoolean(getString(R.string.main_pref_key_switch), false);
+        setActionBarBackground();
+        serviceIntent = new Intent(this, BlockService.class);
 
         // set Incoming Call list
         AutoDataAdapter adapter = new AutoDataAdapter(this, android.R.id.text1);
@@ -53,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 incomingPhoneView.showDropDown();
+                incomingPhoneView.setError(null);
                 return false;
             }
         });
@@ -69,17 +80,21 @@ public class MainActivity extends AppCompatActivity {
      * Handle click Action Button 'Start/Stop Service'
      */
     public void startService(MenuItem item) {
-        startBlockService();
-    }
-
-    private void startBlockService() {
-        Intent i = new Intent(this, BlockService.class);
-        startService(i);
-        int color = getResources().getColor(R.color.green01);
-        if (actionBar != null) {
-            actionBar.setBackgroundDrawable(new ColorDrawable(color));
+        if (isServiceEnabled) {
+            isServiceEnabled = false;
+            stopService(serviceIntent);
+            Toast.makeText(this, R.string.main_service_disabled_message, Toast.LENGTH_LONG).show();
+            Log.d(LOG_TAG, "stopService");
+        } else {
+            isServiceEnabled = true;
+            startService(serviceIntent);
+            Toast.makeText(this, R.string.main_service_enabled_message, Toast.LENGTH_LONG).show();
+            Log.d(LOG_TAG, "startService");
         }
-        Log.d(LOG_TAG, "startService");
+        setActionBarBackground();
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean(getString(R.string.main_pref_key_switch), isServiceEnabled);
+        editor.commit();
     }
 
     /**
@@ -126,6 +141,18 @@ public class MainActivity extends AppCompatActivity {
             Uri uri = ContentUris.withAppendedId(DBContentProvider.CONTENT_ID_URI, rowID);
             getContentResolver().delete(uri, where, null);
             return null;
+        }
+    }
+
+    private void setActionBarBackground() {
+        if (actionBar != null) {
+            int color;
+            if (isServiceEnabled) {
+                color = getResources().getColor(R.color.green01);
+            } else {
+                color = getResources().getColor(R.color.orange01);
+            }
+            actionBar.setBackgroundDrawable(new ColorDrawable(color));
         }
     }
 
