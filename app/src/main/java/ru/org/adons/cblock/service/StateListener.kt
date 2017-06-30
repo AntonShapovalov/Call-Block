05df1f539bf3be4b6835b2f349a453c7dede261f
AntonShapovalov <anton.shapovalov.5@gmail.com>
2017-06-30 19:54:18 +0300

    refactoring to kotlin
    1. convert all app classes to Kotlin! (exclude GreenDao only)
    2. TODO: apply kapt to all tests

--- app/src/main/java/ru/org/adons/cblock/service/StateListener.kt
+++ app/src/main/java/ru/org/adons/cblock/service/StateListener.kt
@@ -11,7 +11,7 @@ import java.util.*
 /**
  * Phone State Listener, used in [BlockService]
  */
-class StateListener(val context: Context, manager: TelephonyManager, val phones: HashSet<String>) : PhoneStateListener() {
+class StateListener(context: Context, manager: TelephonyManager, val phones: HashSet<String>) : PhoneStateListener() {
 
     private val audio: AudioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
@@ -24,8 +24,6 @@ class StateListener(val context: Context, manager: TelephonyManager, val phones:
         log(e.message ?: "getITelephony exception")
         null
     }
-
-
 
     override fun onCallStateChanged(state: Int, incomingNumber: String) {
         super.onCallStateChanged(state, incomingNumber)
